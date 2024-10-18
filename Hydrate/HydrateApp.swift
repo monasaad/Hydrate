d212a85a65e146ad12b010702b18ffa440146fd6
monasaad <monaaa.saad@yahoo.com>
2024-10-18 14:31:42 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Mona on 15/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
